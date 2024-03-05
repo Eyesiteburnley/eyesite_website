c580bfcdb3c04cd419ef56f723a0c779102d6bfe
Hchoudry <102235101+HashimChoudry@users.noreply.github.com>
2024-03-05 22:03:13 +0000

    Initialising project, pages components have been added for the home page

---
+++ components/BookingButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+type ButtonProps={
+    type:'button' | 'submit';
+    title:string;
+    icon?:string;
+    variant:'btn_white';
+}
+
+const BookingButton = ({type, title, icon, variant}: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} cursor-pointer`}
+      type={type}
+    >
+        {icon&& <Image src={icon} alt={title} width={24} height={24}/>}
+        <label className='bold-13 whitespace-nowrap cursor-pointer'>{title}</label>
+    </button>
+  )
+}
+
+export default BookingButton
---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import BookingButton from './BookingButton'
+import { NAV_LINKS } from '@/constants'
+
+
+const Navbar = () => {
+  return (
+    <nav className='border-2 bg-teal-500 flexBetween max-container padding-container relative z-30 py-5'>
+      <Link href="/">
+        <Image src="/white_logo.png" alt="logo" width={100} height={50}/>
+      </Link>
+      <ul className='hidden h-full gap-12 lg:flex'>
+        {NAV_LINKS.map((link)=>(
+          <Link href={link.href} key={link.key} className='regular-16 text-white flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className='lg:flexCenter hidden'>
+        <BookingButton 
+        type="button"
+        title="Booking"
+        icon=""
+        variant="btn_white"
+        />
+      </div>
+
+      <Image 
+      src="menu.svg"
+      alt="menu"
+      width={32}
+      height={32}
+      className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  )
+}
+
+export default Navbar
